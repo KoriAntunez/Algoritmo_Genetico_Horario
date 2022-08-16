a4feca7592d703913508b994c973ca3ea5b2e7a8
KoriAntunez <kori.antunez@unmsm.edu.pe>
2022-08-15 20:47:31 -0500

    commit

--- containers/Generate.py
+++ containers/Generate.py
@@ -1,189 +1,126 @@
-#Importación de libreria
-from PyQt5 import QtCore, QtWidgets
-from components import Database as db, ResourceTracker, ScheduleParser, ScenarioComposer, AlgoritmoGenetico
-from py_ui import Generate as Parent
-from sqlite3 import Binary
-from numpy import mean
-import pickle
-import copy
+# -*- coding: utf-8 -*-
 
-##Función principal que declara todos los elementos que se van a usar en las otras funciones
-class Generate:
-    def __init__(self):
-        self.totalResource = {
-            'cpu': [],
-            'memoria': []
-        }
-        self.tick = 0
-        self.data = {
-            'resultados': [],
-            'aulas': [],
-            'instructores': [],
-            'secciones': [],
-            'reuniones': [],
-            'materias': []
-        }
-        self.topChromosomes = []
-        self.meta = []
-        self.preview = True
-        self.sectionKeys = []
-        composer = ScenarioComposer.ScenarioComposer()
-        composer = composer.getScenarioData()
-        self.data.update(composer)
-        self.dialog = dialog = QtWidgets.QDialog(parent=None)
-        # Initialize custom dialog
-        self.parent = parent = Parent.Ui_Dialog()
-        # Add parent to custom dialog
-        parent.setupUi(dialog)
-        dialog.setWindowFlags(QtCore.Qt.Window | QtCore.Qt.WindowTitleHint | QtCore.Qt.CustomizeWindowHint)
-        self.time = QtCore.QTime(0, 0)
-        self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.updateTime)
-        self.timer.start(1000)
-        self.running = True
-        self.table = parent.tableSchedule
-        parent.btnPause.clicked.connect(self.togglePause)
-        parent.btnStop.clicked.connect(self.stopOperation)
-        parent.chkPreview.clicked.connect(self.togglePreview)
-        parent.cmbSection.clear()
-        for section, details in self.data['secciones'].items():
-            self.sectionKeys.append(section)
-            parent.cmbSection.addItem(details[0])
-        parent.cmbSection.currentIndexChanged.connect(self.changePreview)
-        self.startWorkers()
-        dialog.exec_()
+# Form implementation generated from reading ui file 'generate.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+#
+# WARNING! All changes made in this file will be lost!
 
-# Función Muestra la opción de vista previa
-    def togglePreview(self, state):
-        self.preview = not state
-# Funcion que detalla la generación resumen 
-    def togglePause(self):
-        self.toggleState()
-        self.parent.btnPause.setText('Generación en pausa' if self.running else 'Generacion resumen')
-#Función donde se activa la generación del horario
-    def toggleState(self, state=None):
-        self.running = (not self.running) if state is None else state
-        self.resourceWorker.running = self.running
-        self.geneticAlgorithm.running = self.running
-#Funcion donde se da inicio a los trabajadores
-    def startWorkers(self):
-        self.resourceWorker = ResourceTrackerWorker()
-        self.resourceWorker.signal.connect(self.updateResource)
-        self.resourceWorker.start()
-        self.geneticAlgorithm = AlgoritmoGenetico.AlgoritmoGenetico(self.data)
-        self.geneticAlgorithm.statusSignal.connect(self.updateStatus)
-        self.geneticAlgorithm.detailsSignal.connect(self.updateDetails)
-        self.geneticAlgorithm.dataSignal.connect(self.updateView)
-        self.geneticAlgorithm.operationSignal.connect(self.updateOperation)
-        self.geneticAlgorithm.start()
-#Función donde se actualiza el estado
-    def updateStatus(self, status):
-        self.parent.lblStatus.setText('Status: {}'.format(status))
-#Funcion donde se actualizan los detalles
-    def updateDetails(self, details):
-        self.parent.boxGen.setTitle('Generation #{}'.format(details[0]))
-        self.parent.lblPopulation.setText('Population: {}'.format(details[1]))
-        self.parent.lblMutation.setText('Mutation Rate: {}%'.format(details[2]))
-        self.parent.lblFitness.setText('Average Fitness: {}%'.format(details[3]))
-        self.parent.lblPreviousFitness.setText('Previous Average Fitness: {}%'.format(details[4]))
-        self.parent.lblHighestFitness.setText('Highest Fitness: {}%'.format(details[5]))
-        self.parent.lblLowestFitness.setText('Lowest Fitness: {}%'.format(details[6]))
-#Función donde se actualiza la vista en base a los cromosomas ingresados
-    def updateView(self, chromosomes):
-        chromosomes.reverse()
-        self.topChromosomes = copy.deepcopy(chromosomes)
-        self.changePreview(self.parent.cmbSection.currentIndex())
-#Funcion donde se hace cambio de la vista previa
-    def changePreview(self, index):
-        data = []
-        if not len(self.topChromosomes) or not self.preview:
-            return False
-        secciones = self.topChromosomes[0][0].data['secciones']
-        rawData = self.data
-        materias = secciones[self.sectionKeys[index]]['details']
-        for subject, details in materias.items():
-            if not len(details):
-                continue
-            instructor = '' if not details[1] else rawData['instructores'][details[1]][0]
-            data.append({'color': None, 'text': '{} \n {} \n {}'.format(rawData['materias'][subject][0],
-                                                                        rawData['aulas'][details[0]][0],
-                                                                        instructor),
-                         'instances': [[day, details[3], details[3] + details[4]] for day in details[2]]})
-        self.loadTable(data)
-#Funcion donde se acrualiza la tabla del horario
-    def loadTable(self, data=[]):
-        self.table.reset()
-        self.table.clearSpans()
-        ScheduleParser.ScheduleParser(self.table, data)
-#Funcion donde se actualiza la operacion
-    def updateOperation(self, type):
-        if type == 1:
-            self.stopOperation()
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-#Funcion donde se actualiza el tiempo transcurrido 
-    def updateTime(self):
-        self.time = self.time.addSecs(1)
-        self.parent.lblTime.setText('Tiempo transcurrido: {}'.format(self.time.toString('hh:mm:ss')))
-#Funcion donde se paran las operaciones 
-    def stopOperation(self):
-        self.toggleState(False)
-        self.resourceWorker.terminate()
-        self.resourceWorker.runThread = False
-        self.geneticAlgorithm.terminate()
-        self.timer.stop()
-        if len(self.topChromosomes):
-            self.parent.btnStop.setText('Ver resultados')
-            self.parent.btnStop.clicked.disconnect(self.stopOperation)
-            self.parent.btnStop.clicked.connect(self.dialog.close)
-            self.parent.lblCPU.setText('Uso de CPU: detenido')
-            self.parent.lblmemoria.setText('memoria Uso: Detenido')
-            self.parent.lblStatus.setText('Estado: Detenido')
-            self.totalResource['cpu'] = mean(self.totalResource['cpu'])
-            self.totalResource['memoria'] = mean(self.totalResource['memoria'])
-            self.meta = [[chromosome[1], chromosome[0].fitnessDetails] for chromosome in
-                         self.topChromosomes]
-            conn = db.getConnection()
-            cursor = conn.cursor()
-            cursor.execute('INSERT INTO resultados (content) VALUES (?)', [Binary(
-                pickle.dumps({'datos': [chromosome[0].data for chromosome in self.topChromosomes],
-                              'meta': self.meta,
-                              'tiempo': self.time.toString('hh:mm:ss'),
-                              'recursos': self.totalResource,
-                              'datos sin procesar': self.data},
-                             pickle.HIGHEST_PROTOCOL))])
-            conn.commit()
-            conn.close()
-        else:
-            self.dialog.close()
-#Función donde se actualizan los recursos
-    def updateResource(self, resource):
-        self.tick += 1
-        if self.tick == 3:
-            self.tick = 0
-        else:
-            self.totalResource['cpu'].append(resource[0])
-            self.totalResource['memoria'].append(resource[1][1])
-        self.parent.lblCPU.setText('CPU Usage: {}%'.format(resource[0]))
-        self.parent.lblMemory.setText('Memory Usage: {}% - {} MB'.format(resource[1][0], resource[1][1]))
 
-#Función donde se crea al trabajado de seguimiento de recursos
-class ResourceTrackerWorker(QtCore.QThread):
-    signal = QtCore.pyqtSignal(object)
-    running = True
-    runThread = True
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(716, 553)
+        Dialog.setMaximumSize(QtCore.QSize(716, 553))
+        self.verticalLayout_5 = QtWidgets.QVBoxLayout(Dialog)
+        self.verticalLayout_5.setObjectName("verticalLayout_5")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.groupBox_3 = QtWidgets.QGroupBox(Dialog)
+        self.groupBox_3.setObjectName("groupBox_3")
+        self.horizontalLayout_5 = QtWidgets.QHBoxLayout(self.groupBox_3)
+        self.horizontalLayout_5.setObjectName("horizontalLayout_5")
+        self.cmbSection = QtWidgets.QComboBox(self.groupBox_3)
+        self.cmbSection.setObjectName("cmbSection")
+        self.cmbSection.addItem("")
+        self.cmbSection.addItem("")
+        self.cmbSection.addItem("")
+        self.cmbSection.addItem("")
+        self.cmbSection.addItem("")
+        self.horizontalLayout_5.addWidget(self.cmbSection)
+        self.chkPreview = QtWidgets.QCheckBox(self.groupBox_3)
+        self.chkPreview.setObjectName("chkPreview")
+        self.horizontalLayout_5.addWidget(self.chkPreview)
+        self.horizontalLayout_2.addWidget(self.groupBox_3)
+        self.groupBox_4 = QtWidgets.QGroupBox(Dialog)
+        self.groupBox_4.setObjectName("groupBox_4")
+        self.horizontalLayout_4 = QtWidgets.QHBoxLayout(self.groupBox_4)
+        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
+        self.btnPause = QtWidgets.QPushButton(self.groupBox_4)
+        self.btnPause.setObjectName("btnPause")
+        self.horizontalLayout_4.addWidget(self.btnPause)
+        self.btnStop = QtWidgets.QPushButton(self.groupBox_4)
+        self.btnStop.setObjectName("btnStop")
+        self.horizontalLayout_4.addWidget(self.btnStop)
+        self.horizontalLayout_2.addWidget(self.groupBox_4)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_2)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.boxGen = QtWidgets.QGroupBox(Dialog)
+        self.boxGen.setObjectName("boxGen")
+        self.gridLayout = QtWidgets.QGridLayout(self.boxGen)
+        self.gridLayout.setObjectName("gridLayout")
+        self.lblFitness = QtWidgets.QLabel(self.boxGen)
+        self.lblFitness.setObjectName("lblFitness")
+        self.gridLayout.addWidget(self.lblFitness, 2, 0, 1, 1)
+        self.lblPreviousFitness = QtWidgets.QLabel(self.boxGen)
+        self.lblPreviousFitness.setObjectName("lblPreviousFitness")
+        self.gridLayout.addWidget(self.lblPreviousFitness, 2, 1, 1, 1)
+        self.lblPopulation = QtWidgets.QLabel(self.boxGen)
+        self.lblPopulation.setObjectName("lblPopulation")
+        self.gridLayout.addWidget(self.lblPopulation, 1, 0, 1, 1)
+        self.lblMutation = QtWidgets.QLabel(self.boxGen)
+        self.lblMutation.setObjectName("lblMutation")
+        self.gridLayout.addWidget(self.lblMutation, 1, 1, 1, 1)
+        self.lblHighestFitness = QtWidgets.QLabel(self.boxGen)
+        self.lblHighestFitness.setObjectName("lblHighestFitness")
+        self.gridLayout.addWidget(self.lblHighestFitness, 3, 0, 1, 1)
+        self.lblLowestFitness = QtWidgets.QLabel(self.boxGen)
+        self.lblLowestFitness.setObjectName("lblLowestFitness")
+        self.gridLayout.addWidget(self.lblLowestFitness, 3, 1, 1, 1)
+        self.horizontalLayout.addWidget(self.boxGen)
+        self.groupBox = QtWidgets.QGroupBox(Dialog)
+        self.groupBox.setObjectName("groupBox")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.groupBox)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.lblCPU = QtWidgets.QLabel(self.groupBox)
+        self.lblCPU.setObjectName("lblCPU")
+        self.gridLayout_2.addWidget(self.lblCPU, 2, 0, 1, 1)
+        self.lblTime = QtWidgets.QLabel(self.groupBox)
+        self.lblTime.setObjectName("lblTime")
+        self.gridLayout_2.addWidget(self.lblTime, 0, 0, 1, 2)
+        self.lblMemory = QtWidgets.QLabel(self.groupBox)
+        self.lblMemory.setObjectName("lblMemory")
+        self.gridLayout_2.addWidget(self.lblMemory, 2, 1, 1, 1)
+        self.lblStatus = QtWidgets.QLabel(self.groupBox)
+        self.lblStatus.setObjectName("lblStatus")
+        self.gridLayout_2.addWidget(self.lblStatus, 1, 0, 1, 2)
+        self.horizontalLayout.addWidget(self.groupBox)
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
+        self.verticalLayout_5.addLayout(self.verticalLayout_2)
+        self.tableSchedule = QtWidgets.QTableView(Dialog)
+        self.tableSchedule.setObjectName("tableSchedule")
+        self.verticalLayout_5.addWidget(self.tableSchedule)
 
-    def __init__(self):
-        super().__init__()
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
 
-    def __del__(self):
-        self.wait()
-#Funcion donde se hacen uso de los recursos declarados
-    def run(self):
-        while (self.runThread):
-            self.sleep(1)
-            if self.running is True:
-                cpu = ResourceTracker.getCPUUsage()
-                memoria = ResourceTracker.getMemoryUsage()
-                memoria = [ResourceTracker.getMemoryPercentage(memoria), ResourceTracker.byteToMegabyte(memoria[0])]
-                self.signal.emit([cpu, memoria])
-        return True
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Generate"))
+        self.groupBox_3.setTitle(_translate("Dialog", "Preview"))
+        self.cmbSection.setItemText(0, _translate("Dialog", "Section 1"))
+        self.cmbSection.setItemText(1, _translate("Dialog", "Section 2"))
+        self.cmbSection.setItemText(2, _translate("Dialog", "Section 3"))
+        self.cmbSection.setItemText(3, _translate("Dialog", "Section 4"))
+        self.cmbSection.setItemText(4, _translate("Dialog", "Section 5"))
+        self.chkPreview.setText(_translate("Dialog", "Disable Preview"))
+        self.groupBox_4.setTitle(_translate("Dialog", "Operation"))
+        self.btnPause.setText(_translate("Dialog", "Pause Generation"))
+        self.btnStop.setText(_translate("Dialog", "Stop Generation"))
+        self.boxGen.setTitle(_translate("Dialog", "Generation N"))
+        self.lblFitness.setText(_translate("Dialog", "Average Fitness:"))
+        self.lblPreviousFitness.setText(_translate("Dialog", "Previous Average Fitness:"))
+        self.lblPopulation.setText(_translate("Dialog", "Population:"))
+        self.lblMutation.setText(_translate("Dialog", "Mutation Rate:"))
+        self.lblHighestFitness.setText(_translate("Dialog", "Highest Fitness:"))
+        self.lblLowestFitness.setText(_translate("Dialog", "Lowest Fitness:"))
+        self.groupBox.setTitle(_translate("Dialog", "System"))
+        self.lblCPU.setText(_translate("Dialog", "CPU Usage:"))
+        self.lblTime.setText(_translate("Dialog", "Elapsed Time:"))
+        self.lblMemory.setText(_translate("Dialog", "Memory Usage:"))
+        self.lblStatus.setText(_translate("Dialog", "Status:"))
--- containers/Instructor.py
+++ containers/Instructor.py
@@ -1,145 +1,65 @@
-#Importacion de la librerias
-from PyQt5 import QtWidgets, QtGui
-from components import Database as db, Timetable
-from py_ui import Instructor as Parent
-import json
+# -*- coding: utf-8 -*-
 
-#Clase donde se crea al instructor
-class Instructor:
-    def __init__(self, id):
-        self.id = id
-        self.dialog = dialog = QtWidgets.QDialog()
-        # From the qt_ui generated UI
-        self.parent = parent = Parent.Ui_Dialog()
-        parent.setupUi(dialog)
-        if id:
-            self.fillForm()
-        else:
-            # Create a new instance of timetable
-            self.table = Timetable.Timetable(parent.tableSchedule)
-        parent.btnFinish.clicked.connect(self.finish)
-        parent.btnCancel.clicked.connect(self.dialog.close)
-        dialog.exec_()
-#Funcion donde se muestran de la base de datos los instructores 
-    def fillForm(self):
-        conn = db.getConnection()
-        cursor = conn.cursor()
-        cursor.execute('SELECT name, hours, schedule FROM instructors WHERE id = ?', [self.id])
-        result = cursor.fetchone()
-        conn.close()
-        self.parent.lineEditName.setText(str(result[0]))
-        self.parent.lineEditHours.setText(str(result[1]))
-        # Generate timetable from custom schedule
-        self.table = Timetable.Timetable(self.parent.tableSchedule, json.loads(result[2]))
-#Funcion donde se finaliza el tiempo de trabajo del instructor
-    def finish(self):
-        # Verification of input
-        if not self.parent.lineEditName.text():
-            return False
-        name = self.parent.lineEditName.text()
-        try:
-            hours = int(self.parent.lineEditHours.text())
-            if hours <= 0 or hours > 100 or hours % .5 != 0:
-                return False
-        except:
-            return False
-        data = [name, hours, json.dumps(self.table.getData()), self.id]
-        if not self.id:
-            data.pop()
-        self.insertInstructor(data)
-        self.dialog.close()
-#Funcion donde se hace la insersión de los instructores en los horarios
-    @staticmethod
-    def insertInstructor(data):
-        conn = db.getConnection()
-        cursor = conn.cursor()
-        if len(data) > 3:
-            cursor.execute('UPDATE instructors SET name = ?, hours = ?, schedule = ? WHERE id = ?', data)
-        else:
-            cursor.execute('INSERT INTO instructors (name, hours, schedule) VALUES (?, ?, ?)', data)
-        conn.commit()
-        conn.close()
-        return True
+# Form implementation generated from reading ui file 'instructor.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+#
+# WARNING! All changes made in this file will be lost!
 
-#Clase donde se crea una estructura arbol de los instructores
-class Tree:
-    def __init__(self, tree):
-        self.tree = tree
-        self.model = model = QtGui.QStandardItemModel()
-        model.setHorizontalHeaderLabels(['ID', 'Disponible', 'Nombre', 'Horas', 'Operacion'])
-        tree.setModel(model)
-        tree.setColumnHidden(0, True)
-        model.itemChanged.connect(lambda item: self.toggleAvailability(item))
-        self.display()
-    #Funcion donde se activa  y avilita a los instructores de acuerdo a estadod
-    def toggleAvailability(self, item):
-        # Get ID of toggled instructor
-        id = self.model.data(self.model.index(item.row(), 0))
-        newValue = 1 if item.checkState() == 2 else 0
-        conn = db.getConnection()
-        cursor = conn.cursor()
-        cursor.execute('UPDATE instructors SET active = ?  WHERE id = ?', [newValue, id])
-        conn.commit()
-        conn.close()
-    #Funcion donde se muestra a los instrutores
-    def display(self):
-        # Clear model
-        self.model.removeRows(0, self.model.rowCount())
-        conn = db.getConnection()
-        cursor = conn.cursor()
-        cursor.execute('SELECT id, active, hours, name FROM instructors')
-        result = cursor.fetchall()
-        conn.close()
-        for instr in result:
-            # ID Item
-            id = QtGui.QStandardItem(str(instr[0]))
-            id.setEditable(False)
-            # Availability Item
-            availability = QtGui.QStandardItem()
-            availability.setCheckable(True)
-            availability.setCheckState(2 if instr[1] == 1 else 0)
-            availability.setEditable(False)
-            # Hours Item
-            hours = QtGui.QStandardItem(str(instr[2]))
-            hours.setEditable(False)
-            # Name Item
-            name = QtGui.QStandardItem(instr[3])
-            name.setEditable(False)
-            # Edit Item / Container for operation buttons
-            edit = QtGui.QStandardItem()
-            edit.setEditable(False)
-            # Append items to model
-            self.model.appendRow([id, availability, name, hours, edit])
-            # Create a widget group for edit and delete buttons
-            frameEdit = QtWidgets.QFrame()
-            btnEdit = QtWidgets.QPushButton('Edit', frameEdit)
-            btnEdit.clicked.connect(lambda state, id=instr[0]: self.edit(id))
-            btnDelete = QtWidgets.QPushButton('Delete', frameEdit)
-            btnDelete.clicked.connect(lambda state, id=instr[0]: self.delete(id))
-            frameLayout = QtWidgets.QHBoxLayout(frameEdit)
-            frameLayout.setContentsMargins(0, 0, 0, 0)
-            frameLayout.addWidget(btnEdit)
-            frameLayout.addWidget(btnDelete)
-            # Append the widget group to edit item
-            self.tree.setIndexWidget(edit.index(), frameEdit)
-    #Funcion donde se puede editar a los instrutores
-    def edit(self, id):
-        Instructor(id)
-        self.display()
-    #Funcion donde se borra a los instructores
-    def delete(self, id):
-        # Show confirm model
-        confirm = QtWidgets.QMessageBox()
-        confirm.setIcon(QtWidgets.QMessageBox.Warning)
-        confirm.setText('Are you sure you want to delete this entry?')
-        confirm.setWindowTitle('Confirm Delete')
-        confirm.setStandardButtons(QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
-        result = confirm.exec_()
-        # 16384 == Confirm
-        if result == 16384:
-            conn = db.getConnection()
-            cursor = conn.cursor()
-            cursor.execute('DELETE FROM instructors WHERE id = ?', [id])
-            conn.commit()
-            conn.close()
-            self.display()
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(716, 553)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(Dialog.sizePolicy().hasHeightForWidth())
+        Dialog.setSizePolicy(sizePolicy)
+        Dialog.setMinimumSize(QtCore.QSize(716, 553))
+        Dialog.setMaximumSize(QtCore.QSize(716, 553))
+        Dialog.setFocusPolicy(QtCore.Qt.NoFocus)
+        Dialog.setModal(False)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Dialog)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.gridLayout = QtWidgets.QGridLayout()
+        self.gridLayout.setSizeConstraint(QtWidgets.QLayout.SetMinimumSize)
+        self.gridLayout.setObjectName("gridLayout")
+        self.lblName = QtWidgets.QLabel(Dialog)
+        self.lblName.setObjectName("lblName")
+        self.gridLayout.addWidget(self.lblName, 0, 0, 1, 1)
+        self.lineEditName = QtWidgets.QLineEdit(Dialog)
+        self.lineEditName.setObjectName("lineEditName")
+        self.gridLayout.addWidget(self.lineEditName, 0, 1, 1, 1)
+        self.lblHours = QtWidgets.QLabel(Dialog)
+        self.lblHours.setObjectName("lblHours")
+        self.gridLayout.addWidget(self.lblHours, 0, 2, 1, 1)
+        self.lineEditHours = QtWidgets.QLineEdit(Dialog)
+        self.lineEditHours.setObjectName("lineEditHours")
+        self.gridLayout.addWidget(self.lineEditHours, 0, 3, 1, 1)
+        self.verticalLayout.addLayout(self.gridLayout)
+        self.tableSchedule = QtWidgets.QTableView(Dialog)
+        self.tableSchedule.setObjectName("tableSchedule")
+        self.verticalLayout.addWidget(self.tableSchedule)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.btnFinish = QtWidgets.QPushButton(Dialog)
+        self.btnFinish.setObjectName("btnFinish")
+        self.horizontalLayout.addWidget(self.btnFinish)
+        self.btnCancel = QtWidgets.QPushButton(Dialog)
+        self.btnCancel.setObjectName("btnCancel")
+        self.horizontalLayout.addWidget(self.btnCancel)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Instructor"))
+        self.lblName.setText(_translate("Dialog", "Name"))
+        self.lblHours.setText(_translate("Dialog", "Available Hours"))
+        self.btnFinish.setText(_translate("Dialog", "Finish"))
+        self.btnCancel.setText(_translate("Dialog", "Cancel"))
